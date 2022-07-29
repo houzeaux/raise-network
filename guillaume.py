f3c89f5ad95b304d1caf8114525d6e4c8cee9325
guillaume houzeaux <guillaume.houzeaux@bsc.es>
2022-07-29 14:27:01 +0200

    test

--- guillaume.py
+++ guillaume.py
@@ -16,9 +16,9 @@ main_panel.markdown(unblur_header,unsafe_allow_html=True)
 
 # doing the upper menu
 #
-#sidebar.image("imgs/logo.png",width=200)
-sidebar.image("imgs/raise.png",width=100)
+sidebar.image("imgs/raise.png",width=200)
 sidebar.write("## T6.2 Establishment of a European RAISE network ")
+sidebar.write("Visualize our activities in creating a European network for AI and HPC")
 sidebar.write("The task contributors represent the responsible institutions per participating country. In this task, they establish a European AISee network by implementing country-dependent access and contact nodes. They connect the activities of AISee to those of their network, national, European and international. That is, FZJ establishes connections, e.g., to GCS, HAICU, and IRT in Germany as well as to PRACE together with BSC at European level. UOI connects as a hub to the Nordic countries with NeiC and COST Action CA18203, and together with RTU to EOSC-NORDIC. CYI acts as hub for the EM, has strong links with SESAME, with the Balkans through NI4OS-Europe, as well as links with the oil and gas industry (the Delphi consortium, see Sec. 4.2.2). It extends its reach to other industries through the SimEA ERA Chair project. RTU acts as a hub for the Baltic Region through SESAME Net. RWTH employs its channels to Gauss Alliance and HPC.NRW. CERN connects to INFN, Siemens, and E4 Computer Engineering. CERFACS establishes connections with academics (ISAE-Supaero, IRT, ONERA) and industrial partners (AIRBUS, CNES, Safran) among other within the interdisciplinary institute ANITI and the Helios multi- laboratory workgroup. FM exploits its industrial partner network. Partners involved in other CoEs or European projects establish corresponding connections, i.e., to EXCELLERAT, POP, EoCoE-II, EPI, DEEP, EMME-CARE, etc. (see. Sec. 1.3.3, Tab. 4). Furthermore, connections to ETP4HPC are established. ")
 
 
