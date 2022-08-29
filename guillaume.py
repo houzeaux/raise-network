df85b419a3b494bfcdd250fa9b5be13ba0dbf098
guillaume <guillaume.houzeaux@bsc.es>
2022-08-29 17:00:07 +0200

    hello

--- guillaume.py
+++ guillaume.py
@@ -45,10 +45,10 @@ if col == "Region":
     if row == "All items":
         for name,content in data.iterrows():
             main_panel.markdown("#####  " + str(content[1]) + ", " + str(content[0]) +':' )
-            main_panel.markdown( "* Activity:    "+str(content[2])+"  " )
-            main_panel.markdown( "* Institution: "+str(content[3])+"  " )
-            main_panel.markdown( "* Community:   "+str(content[4]) )
-            main_panel.markdown( "* Description: "+ name +": " )
+            main_panel.markdown( "Activity:    "+str(content[2])+"  " )
+            main_panel.markdown( "Institution: "+str(content[3])+"  " )
+            main_panel.markdown( "Community:   "+str(content[4]) )
+            main_panel.markdown( "Description: "+ name +": " )
     else:
         for name,content in data.loc[data[col]==row,:].iterrows():
             main_panel.markdown("#####  " + str(content[1]) + ", " + str(content[0]) +':' )
