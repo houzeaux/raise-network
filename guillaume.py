63be6044ae1bcd9dedbb5379777fc3dd5aaa5c0e
guillaume <guillaume.houzeaux@bsc.es>
2022-08-29 16:21:09 +0200

    hello

--- guillaume.py
+++ guillaume.py
@@ -49,9 +49,12 @@ if col == "Region":
             main_panel.markdown( "Activity:    "+str(content[2]) )
             main_panel.markdown( "Institution: "+str(content[3]) )
             main_panel.markdown( "Community:   "+str(content[4]) )
-            main_panel.markdown( "Activity:    "+str(content[2]) 
-                                 "Institution: "+str(content[3])
-                                 "Community:   "+str(content[4]) )
+            st.markdown("""Hello
+
+This is a cheat sheet built with the most common charts that I use in the data analysis for the projects.
+
+The idea is to have a quick and useful sheet where anyone can copy and past the code (1 line command usually) to generate the charts below.
+""")
     else:
         for name,content in data.loc[data[col]==row,:].iterrows():
             main_panel.markdown("######  " + str(content[1]) + ", " + str(content[0]) +':' )
