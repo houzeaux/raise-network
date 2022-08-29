3ffeffddb677565c5de1c99c61945d1dfa7e58da
guillaume <guillaume.houzeaux@bsc.es>
2022-08-29 17:10:15 +0200

    hello

--- guillaume.py
+++ guillaume.py
@@ -49,9 +49,6 @@ if col == "Region":
             main_panel.markdown( "* Institution: "+str(content[3]) )
             main_panel.markdown( "* Community:   "+str(content[4]) )
             main_panel.markdown( "* Description: "+ name +": " )
-            main_panel.markdown( """caca
-
-                                  pipi""")
     else:
         for name,content in data.loc[data[col]==row,:].iterrows():
             main_panel.markdown("#####  " + str(content[1]) + ", " + str(content[0]) +':' )
