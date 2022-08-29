e0e04373a3132fcebd52522f2e6d487004f390b6
guillaume <guillaume.houzeaux@bsc.es>
2022-08-29 16:24:55 +0200

    hello

--- guillaume.py
+++ guillaume.py
@@ -44,7 +44,7 @@ row = selector_rows.selectbox(
 if col == "Region":
     if row == "All rows":
         for name,content in data.iterrows():
-            main_panel.markdown("######  " + str(content[1]) + ", " + str(content[0]) +':' )
+            main_panel.markdown("#####  " + str(content[1]) + ", " + str(content[0]) +':' )
             main_panel.markdown( " "+ name +": " )
             main_panel.markdown( "Activity:    "+str(content[2]) )
             main_panel.markdown( "Institution: "+str(content[3]) )
@@ -58,7 +58,7 @@ The idea is to have a quick and useful sheet where anyone can copy and past the 
 """)
     else:
         for name,content in data.loc[data[col]==row,:].iterrows():
-            main_panel.markdown("######  " + str(content[1]) + ", " + str(content[0]) +':' )
+            main_panel.markdown("#####  " + str(content[1]) + ", " + str(content[0]) +':' )
             main_panel.markdown( " "+ name +": " )
             main_panel.markdown( "Activity:    "+str(content[2]) )
             main_panel.markdown( "Institution: "+str(content[3]) )
@@ -66,13 +66,13 @@ The idea is to have a quick and useful sheet where anyone can copy and past the 
 else:
     if row == "All rows":
         for name,content in data.iterrows():
-            main_panel.markdown("######  " + str(content[1]) + ", " + str(content[0]) +':' )
+            main_panel.markdown("#####  " + str(content[1]) + ", " + str(content[0]) +':' )
             main_panel.markdown( " "+ name +": " )
             main_panel.markdown( "Institution: "+str(content[3]) )
             main_panel.markdown( "Community:   "+str(content[4]) )
     else:
         for name,content in data.loc[data[col]==row,:].iterrows():
-            main_panel.markdown("######  " + str(content[1]) + ", " + str(content[0]) +':' )
+            main_panel.markdown("#####  " + str(content[1]) + ", " + str(content[0]) +':' )
             main_panel.markdown( " "+ name +": " )
             main_panel.markdown( "Institution: "+str(content[3]) )
             main_panel.markdown( "Community:   "+str(content[4]) )
