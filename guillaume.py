f2561232caa0ffb957d047ce782c6e224d978d65
guillaume <guillaume.houzeaux@bsc.es>
2022-08-29 16:51:00 +0200

    hello

--- guillaume.py
+++ guillaume.py
@@ -35,20 +35,20 @@ col = selector_columns.selectbox(
         ('Region','Activity')
         )
 datafilter = data[col].unique()
-rows = ["All rows"]+list(datafilter)
+rows = ["All items"]+list(datafilter)
 row = selector_rows.selectbox(
         'Select an item',
         rows
         )
 
 if col == "Region":
-    if row == "All rows":
+    if row == "All items":
         for name,content in data.iterrows():
             main_panel.markdown("#####  " + str(content[1]) + ", " + str(content[0]) +':' )
             main_panel.markdown( "Activity:    "+str(content[2]) )
             main_panel.markdown( "Institution: "+str(content[3]) )
             main_panel.markdown( "Community:   "+str(content[4]) )
-            main_panel.markdown( "Description  "+ name +": " )
+            main_panel.markdown( "Description: "+ name +": " )
     else:
         for name,content in data.loc[data[col]==row,:].iterrows():
             main_panel.markdown("#####  " + str(content[1]) + ", " + str(content[0]) +':' )
@@ -57,7 +57,7 @@ if col == "Region":
             main_panel.markdown( "Community:   "+str(content[4]) )
             main_panel.markdown( "Description: "+ name +": " )
 else:
-    if row == "All rows":
+    if row == "All items":
         for name,content in data.iterrows():
             main_panel.markdown("#####  " + str(content[1]) + ", " + str(content[0]) +':' )
             main_panel.markdown( "Institution: "+str(content[3]) )
