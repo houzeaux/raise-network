8506071ded32de37d6d17aa04d3ea075c4faa556
guillaume <guillaume.houzeaux@bsc.es>
2022-08-30 09:38:10 +0200

    hello

--- guillaume.py
+++ guillaume.py
@@ -20,11 +20,11 @@ st.markdown(
     """
     <style>
     [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
-        width: 400px;
+        width: 450px;
     }
     [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
-        width: 400px;
-        margin-left: -400px;
+        width: 450px;
+        margin-left: -450px;
     }
     </style>
     """,
