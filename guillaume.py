ab00782d712bd43f001bae154656c0e6d693d6af
guillaume <guillaume.houzeaux@bsc.es>
2022-08-30 09:51:54 +0200

    hello

--- guillaume.py
+++ guillaume.py
@@ -101,7 +101,7 @@ if col == "Activity":
             main_panel.markdown( "* Description: "+ "*" + name + "*" )
 else:
     if row == "All items":
-        main_panel.markdown("###  All activities" )        
+        main_panel.markdown("###  All institutions and companies" )        
         main_panel.markdown( "  " )        
         for name,content in data.iterrows():
             main_panel.markdown("#####  " + str(content[1]) + ", " + str(content[0]) )
@@ -111,10 +111,9 @@ else:
             main_panel.markdown( "* Period:      "+str(content[5]) )
             main_panel.markdown( "* Description: "+ "*" + name + "*" )
     else:
-        main_panel.markdown( "###  Activity: " + row )        
+        main_panel.markdown( "###  Institution/company: " + row )        
         for name,content in data.loc[data[col]==row,:].iterrows():
             main_panel.markdown("#####  " + str(content[1]) + ", " + str(content[0]) )
-            main_panel.markdown( "* Institution: "+str(content[3]) )
             main_panel.markdown( "* Community:   "+str(content[4]) )
             main_panel.markdown( "* Period:      "+str(content[5]) )
             main_panel.markdown( "* Description: "+ "*" + name + "*" )
